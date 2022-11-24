fe853f8d097733c15a4a9409b2d8170015b5c299
hyimred <ptrk.csak@gmail.com>
2022-11-24 15:38:11 +0100

    Start

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.ptrkcsak.csakpatrik_javafxrestclientdolgozat {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.ptrkcsak.csakpatrik_javafxrestclientdolgozat to javafx.fxml;
+    exports com.ptrkcsak.csakpatrik_javafxrestclientdolgozat;
+}
